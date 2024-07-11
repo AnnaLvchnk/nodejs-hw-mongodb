72ee3a9c031d8b95c6786a7e09115eb641c6839e
AnnaLvchnk <nlvcnk@gmail.com>
2024-07-11 15:24:16 +0200

    correction env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,13 +1,14 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 
-dotenv.config();
-
-export function env(name, defaultValue) {
+const env = (name, defaultValue) => {
   const value = process.env[name];
+  if (value) {
+    return value;
+  }
+  if (defaultValue) {
+    return defaultValue;
+  }
+  throw new Error(`Missing: process.env[${name}]`);
+};
 
-  if (value) return value;
-
-  if (defaultValue) return defaultValue;
-
-  throw new Error(`Missing: process.env['${name}'].`);
-}
+export default env;
