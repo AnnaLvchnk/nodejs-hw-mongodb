38f9b11c4766e461b0bbfa51350f98eb1a39e18b
AnnaLvchnk <nlvcnk@gmail.com>
2024-07-11 14:54:29 +0200

    fixed errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,18 +1,9 @@
-import { HttpError } from 'http-errors';
+export const errorHandler = (error, req, res) => {
+  const { status = 500, message = 'Something went wrong' } = error;
 
-export const errorHandler = (err, req, res) => {
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err,
-    });
-    return;
-  }
-
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
+  res.status(status).json({
+    status,
+    message,
+    data: error.data,
   });
 };
